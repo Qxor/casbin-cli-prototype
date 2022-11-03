49050d5aec67eacdfaa514d47bd982330bfefaca
Qxor <tabolin_andrey@mail.ru>
2022-11-03 21:37:45 +0300

    change arguments

--- src/enforcers/rbacCasbinEnforcer.js
+++ src/enforcers/rbacCasbinEnforcer.js
@@ -1,7 +1,7 @@
 import { newEnforcer } from "casbin";
 
-async function enforce(model, source, ...args) {
-  const e = await newEnforcer(model,source);
+async function enforce(model, adapter, ...args) {
+  const e = await newEnforcer(model, adapter);
   const result = await e.enforce(...args);
 
   return result;
